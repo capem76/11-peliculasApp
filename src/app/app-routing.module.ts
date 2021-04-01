3ffc0875cd9cc4e97031371f6b121925e12a03cd
capem76 <capem76@hotmail.com>
2021-04-01 17:16:54 +0200

    Revert "update angular core"
    This reverts commit e03d75fd7f1edb768a3682b83bcca0f64cd112f3.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -20,7 +20,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes, { anchorScrolling: 'enabled', relativeLinkResolution: 'legacy' })],
+  imports: [RouterModule.forRoot(routes, {anchorScrolling: 'enabled'})],
   exports: [RouterModule]
 })
 
